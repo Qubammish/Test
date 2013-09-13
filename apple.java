1dcc336ceee06c7884d8b6f597a4f1a69c6ccb08
Ammishaddai Ofori <thepapaworld@gmail.com>
2013-09-13 11:03:18 +0000

    Apple file

---
+++ apple.java
@@ -0,0 +1,10 @@
+class apple{
+	public static void main(String[] args) {
+	int age=23;
+	String dude= "Very tall";
+	String name= "Ammishaddai Ofori";
+	double height= 2.1;
+
+	System.out.println("Hey! Apple! You are " +dude+ " for a young man " +height+ " tall!");
+	}
+}
